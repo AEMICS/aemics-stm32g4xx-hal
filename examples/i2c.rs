742a219ad3df10af8f865e7b562cfcc47aad435b
akker <w.vandenakker@aemics.nl>
2024-05-15 10:58:06 +0200

    Tested and fixed I2C implementation for HAL 1.0.0

--- examples/i2c.rs
+++ examples/i2c.rs
@@ -3,6 +3,19 @@
 #![no_main]
 #![no_std]
 
+//! I2C example
+//!
+//! This example shows how to use the read and write functionality of the I2C API.
+//! The example assumes the usage of an EEPROM chip (24AA04 4K I2C Serial EEPROM)
+//!
+//! This example uses 7-bit addressing (the default for I2C). However, the [`I2C API`](aemics_hal::hal_api::i2c) also supports 10-bit addressing.
+//!
+//! Using the read/write functionality remains the same across any device.
+//!
+//! See [`examples/i2c_transaction.rs`](C:\Users\wybre\Documents\Saxion\YearFour\Graduation\projects\stm32g4xx_hal\examples\i2c_transaction.rs) for a more complex setup using the [`transaction`](I2c::transaction) functionality.
+//!
+
+
 use aemics_stm32g4xx_hal as aemics_hal;
 
 use aemics_hal::preludes::default::*;
@@ -16,6 +29,7 @@ fn main() -> ! {
 
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
     let cp = cortex_m::Peripherals::take().unwrap();
+
     let mut rcc = dp.RCC.constrain();
     let gpioc = dp.GPIOC.split(&mut rcc);
 
@@ -29,22 +43,25 @@ fn main() -> ! {
     //  .I2C1
     //   .i2c(sda, scl, Config::with_timing(0x3042_0f13), &mut rcc);
 
-    let gpiob = dp.GPIOB.split(&mut rcc);
-
-    //Grab pin B7 and convert it to a push-pull output pin. This is the pin connected to the LED on the PYglet board.
-    let mut led = gpiob.pb7.into_push_pull_output();
-
-
     let mut delay = cp.SYST.delay(&rcc.clocks);
 
-    let buf: [u8; 2] = [0, 1];
+    let write_buf: [u8; 4] = [0, 0, 0, 0];
+    let mut read_buf: [u8; 10] = [0; 10];
     loop {
-        match i2c.write(0b1010001_u8, &buf) {
-            Ok(_) => {
-                led.toggle().unwrap();
-                delay.delay_ms(100);
-            }
-            Err(err) => info!("error: {:?}", err),
+        match i2c.write(0b1010000_u8, &write_buf) {
+            Ok(_) => info!("Ok"),
+            Err(err) => {
+                info!("error: {:?}", err);
+            },
+        }
+
+        delay.delay_ms(5); //Delay 5 milliseconds to allow the EEPROM chip to do its page write internal write process.
+
+        match i2c.read(0b1010000_u8, &mut read_buf) {
+            Ok(_) => info!("Ok"),
+            Err(err) => {
+                info!("error: {:?}", err);
+            },
         }
     }
 }
